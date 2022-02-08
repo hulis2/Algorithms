b473ebf31fb4a0877582b153b39d70a13e919c61
hong <92391298+hulis2@users.noreply.github.com>
2022-02-08 11:49:43 +0900

    Create

---
+++ src/p01/HelloWorld.java
@@ -0,0 +1,14 @@
+package p01;
+
+/*
+백준 1단계 입출력 과 사칙연산 문제 - Hello World 출력
+작성자 : 박철홍
+작성일 : 2022.02.08
+*/
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ src/p01/WeLoveKriii.java
@@ -0,0 +1,15 @@
+package p01;
+
+/*
+백준 1단계 입출력 과 사칙연산 문제 - 강한친구 대한육군 두 줄 출력
+작성자 : 박철홍
+작성일 : 2022.02.08
+*/
+
+public class WeLoveKriii {
+
+	public static void main(String[] args) {
+		System.out.print("강한친구 대한육군 \n강한친구 대한육군");
+	}
+
+}
